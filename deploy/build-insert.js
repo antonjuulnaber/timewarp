c8628eb8b3732207720ade52db5778a0725dca7c
Anton Juul-Naber <anton@juul-naber.dk>
2019-10-16 00:46:36 +0200

    Test

--- deploy/build-insert.js
+++ deploy/build-insert.js
@@ -18,6 +18,7 @@ module.exports = {
 		if(rpl_sw_cache_id.hasChanged === true){
 			c.log("Inserted new serviceworker cache id", true);
 		}else{
+			c.log(rpl_sw_cache_id.file, false);
 			c.fail("Could not insert new  serviceworker cache id");
 		}
 	}
