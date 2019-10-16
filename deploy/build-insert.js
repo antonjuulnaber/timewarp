7ca8fac29ebd100ded8ad188733093f8c1091602
Anton Juul-Naber <anton@juul-naber.dk>
2019-10-16 11:37:47 +0200

    Test

--- deploy/build-insert.js
+++ deploy/build-insert.js
@@ -12,16 +12,19 @@ module.exports = {
 		c.log(c.path("../js/sw.js"));
 		
 		const rpl_sw_cache_id = replace({
-			files: c.path("../js/sw.js"),
+			/*files: c.path("../js/sw.js"),*/
+			files: c.path("/home/travis/build/antonjuulnaber/timewarp/js/sw.js"),
 			from: /"!travis_insert_id!"/g,
 			to: "\"cache-" + crypto.randomBytes(10).toString('hex') + "\""
+		}).catch(e => {
+			c.fail("Could not insert new serviceworker cache id: " + e);
 		});
 		
 		if(rpl_sw_cache_id.hasChanged === true){
 			c.log("Inserted new serviceworker cache id", true);
 		}else{
 			c.log(rpl_sw_cache_id.file, false);
-			c.fail("Could not insert new  serviceworker cache id");
+			c.fail("Could not insert new serviceworker cache id");
 		}
 	}
 	
