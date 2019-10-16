a7eff81de44f6c231bad702f07fa11025994a48d
Anton Juul-Naber <anton@juul-naber.dk>
2019-10-16 15:52:27 +0200

    Test

--- deploy/run-build.js
+++ deploy/run-build.js
@@ -5,5 +5,5 @@ const c = require(path.join(__dirname, "controls.js"));
 const insert = require(c.path("build-insert.js"));
 const minify = require(c.path("build-minify.js"));
 
-insert.run();
-minify.run();
+await insert.run();
+await minify.run();
