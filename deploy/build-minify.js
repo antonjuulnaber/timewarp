91e5bccc33a3f2919c486fa39a4fae5cfc58d831
Anton Juul-Naber <anton@juul-naber.dk>
2019-10-16 13:54:13 +0200

    Revert stupid changes

--- deploy/build-minify.js
+++ deploy/build-minify.js
@@ -3,49 +3,48 @@
 module.exports = {
 	
 	run: () => {
-		const path = require("path");
-		const c = require(path.join(__dirname, "controls.js"));
-		const site_root = c.path("..") + "/";
+		const site_root = "/home/travis/build/antonjuulnaber/timewarp/";
 
 		const fs = require("fs");
+		const path = require('path');
 		const rimraf = require("rimraf");
 		const minify = require("minify");
+		const c = require(site_root + "deploy/console.js");
 
 
 		const remove_dirs = ["deploy"];
 		const remove_files = [".travis.yml"];
 
-		const minify_dirs = ["", "css", "js", "data"];
+		//Must have slashes after, and not before
+		const minify_dirs = ["", "css/", "js/", "data/"];
 
 
 		for(const dir of remove_dirs){
 		  c.log("Removing /" + dir);
-		  rimraf(path.join(site_root + dir), [], e => {if(e) c.log("Could not remove file: " + e, false);});
+		  rimraf(site_root + dir, [], e => {if(e) c.log("Could not remove file: " + e, false);});
 		}
 
 		for(const file of remove_files){
 		  c.log("Removing " + file);
-		  fs.unlink(path.join(site_root, file), e => {if(e) c.log("Could not remove file: " + e, false);});
+		  fs.unlink(site_root + file, e => {if(e) c.log("Could not remove file: " + e, false);});
 		}
 
 		for(const dir of minify_dirs){
-			console.log(path.join(site_root, dir));
-			console.log(site_root + dir);
-			fs.stat(path.join(site_root, dir), e => {
+			fs.stat(site_root + dir, e => {
 				if(!e){
-					fs.readdir(path.join(site_root, dir), (e, files) => {
+					fs.readdir(site_root + dir, (e, files) => {	
 						for(const file of files){
 							let p = path.extname(file).toLowerCase();
 							if(p === ".html" || p === ".css" || p === ".js"){
-								c.log("Minifying " + path.join(dir, file));
-								minify(path.join(site_root, dir, file)).then(minified => {
-									fs.writeFile(path.join(site_root, dir, file), minified, e => {if(e) c.fail(e);});
+								c.log("Minifying " + dir + file);
+								minify(site_root + dir + file).then(minified => {
+									fs.writeFile(site_root + dir + file, minified, e => {if(e) c.fail(e);});
 								}).catch(e => {c.fail(e);});
 							}else if(p === ".json"){
-								c.log("Minifying " + path.join(dir, file));
-								fs.readFile(path.join(site_root, dir, file), "utf-8", (e, data) => {
+								c.log("Minifying " + dir + file);
+								fs.readFile(site_root + dir + file, "utf-8", (e, data) => {
 									if(e) c.fail(e);
-									fs.writeFile(path.join(site_root, dir, file), JSON.stringify(JSON.parse(data)), e => {if(e) c.fail(e);});
+									fs.writeFile(file, JSON.stringify(JSON.parse(data)), e => {if(e) c.fail(e);});
 								});
 							}
 						}
