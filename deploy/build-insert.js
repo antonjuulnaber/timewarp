b70efbc6e3a9a67e9fdd5d4006f649d314a6c195
Anton Juul-Naber <anton@juul-naber.dk>
2019-10-16 12:18:40 +0200

    Change replace function

--- deploy/build-insert.js
+++ deploy/build-insert.js
@@ -6,13 +6,18 @@ module.exports = {
 		const path = require("path");
 		const c = require(path.join(__dirname, "controls.js"));
 		
-		const rt = require("replace-in-file");
 		const crypto = require("crypto");
 		
 		c.log(c.path("../js/sw.js"));
 		
+		fs.readFile(c.path("../js/sw.js"), "utf-8", (e, data) => {
+			if(e) c.fail(e);
+			fs.writeFile(file, data.replace("\"!travis_insert_id!\"", "\"cache-" + crypto.randomBytes(10).toString('hex') + "\"");), e => {if(e) c.fail(e);});
+		});
+		
+		/*
 		const rpl_sw_cache_id = rt({
-			/*files: c.path("../js/sw.js"),*/
+			files: c.path("../js/sw.js"),
 			files: c.path("/home/travis/build/antonjuulnaber/timewarp/js/sw.js"),
 			from: /"!travis_insert_id!"/g,
 			to: "\"cache-" + crypto.randomBytes(10).toString('hex') + "\""
@@ -25,9 +30,10 @@ module.exports = {
 		if(rpl_sw_cache_id.hasChanged === true){
 			c.log("Inserted new serviceworker cache id", true);
 		}else{
-			c.log(rpl_sw_cache_id[0].hasChanged, false);
+			c.log(rpl_sw_cache_id.tostring, false);
 			c.fail("Could not insert new serviceworker cache id");
 		}
+		*/
 	}
 	
 }
